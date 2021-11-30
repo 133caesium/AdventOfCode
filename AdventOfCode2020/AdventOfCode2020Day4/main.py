f242ce25d55d58826fd2f29981c36500b833db20
nber106 <nber106@aucklanduni.ac.nz>
2021-12-01 08:44:06 +1300

    working on day4 2020 in python

--- AdventOfCode2020/AdventOfCode2020Day4/main.py
+++ AdventOfCode2020/AdventOfCode2020Day4/main.py
@@ -1,22 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def load_input():
-    input_lines = []
-    with open('./resources/input.txt') as file:
-        input_lines = file.readlines()
-    return input_lines
-    #
-    # count = 0
-    # for line in lines:
-    #     count += 1
-    #     print(f'line {count}: {line}')
-
 def main():
-    print(load_input())
+    print("Main Method")
 
 if __name__ == '__main__':
     main()
--- AdventOfCode2020/AdventOfCode2020Day4/passport_module.py
+++ AdventOfCode2020/AdventOfCode2020Day4/passport_module.py
@@ -1,4 +1,32 @@
 # I TRIED TO USE SLOTS BUT IT DOESN'T WORK WELL.
+
+class Passport(object):
+    def __init__(self, **kwargs):
+        self.__byr = kwargs.get('byr')
+        self.__iyr = kwargs.get('iyr')
+        self.__eyr = kwargs.get('eyr')
+        self.__hgt = kwargs.get('hgt')
+        self.__hcl = kwargs.get('hcl')
+        self.__ecl = kwargs.get('ecl')
+        self.__pid = kwargs.get('pid')
+        self.__cid = kwargs.get('cid')
+
+    def is_valid(self):
+        properties = [self.__byr, self.__iyr, self.__eyr, self.__hgt, self.__hcl, self.__ecl, self.__pid, self.__cid]
+        for value in properties:
+            if value==None:
+                return False
+        return True
+
+#
+#
+# class Passport_from_dict(Passport):
+#     __slots__ = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
+#     def __init__(self, **kwargs):
+#         super().__init__(**kwargs)
+
+
+
 # class Passport(object):
 #     __slots__ = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
 #     def __init__(self, **kwargs):
