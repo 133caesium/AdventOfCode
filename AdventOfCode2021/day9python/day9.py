8fafa1315b6aaf25aad55c4ebc56a9b74d63fb99
nber106 <nber106@aucklanduni.ac.nz>
2022-01-06 09:01:04 +1300

    solved the first part of day 9 but offline so unable to check

--- AdventOfCode2021/day9python/day9.py
+++ AdventOfCode2021/day9python/day9.py
@@ -27,22 +27,73 @@ class LineParser:
 class SmokeMap:
     def __init__(self, parser):
         self.__smoke_map = np.matrix(parser.get_initial_state())
-        self.__lowpoint_map = self.map_lowpoints()
+        self.__lowpoint_map = np.zeros(self.__smoke_map.shape, dtype=np.int32)
+        self.__y_max = self.__smoke_map.shape[0] - 1
+        self.__x_max = self.__smoke_map.shape[1] - 1
+        self.map_lowpoints()
         self.__lowpoint_count = 0
-        # self.__y_dimension = parser.get_max_y()+1
-        # self.__hydrothermal_map_matrix = np.zeros((self.__x_dimension, self.__y_dimension), dtype=np.int32)
-        # self.__line_coordinates = parser.get_lines()
 
-    def get_map(self):
+
+    def get_smoke_map(self):
         return self.__smoke_map
 
+    def get_lowpoint_map(self):
+        return self.__lowpoint_map
+
     def map_lowpoints(self):
-        new_map = np.zeros(self.__smoke_map.shape, dtype=np.int32)
-        return new_map
+        self.map_corners()
+        self.map_edges()
+        self.map_center()
 
+    def map_center(self):
+        scan_x = 1
+        while scan_x < self.__x_max:
+            scan_y = 1
+            while scan_y < self.__y_max:
+                if self.check_above_higher(scan_y, scan_x) and self.check_left_higher(scan_y, scan_x) and self.check_right_higher(scan_y, scan_x) and self.check_below_higher(scan_y, scan_x):
+                    self.__lowpoint_map[scan_y, scan_x] = self.__smoke_map[scan_y, scan_x] + 1
+                scan_y += 1
+            scan_x += 1
 
-    def map(self):
-        pass
+    def map_edges(self):
+        scan_x = 1
+        while scan_x < self.__x_max:
+            if self.check_left_higher(0, scan_x) and self.check_right_higher(0, scan_x) and self.check_below_higher(0, scan_x):
+                self.__lowpoint_map[0, scan_x] = self.__smoke_map[0, scan_x] + 1
+            if self.check_left_higher(self.__y_max, scan_x) and self.check_right_higher(self.__y_max, scan_x) and self.check_above_higher(self.__y_max, scan_x):
+                self.__lowpoint_map[self.__y_max, scan_x] = self.__smoke_map[self.__y_max, scan_x] + 1
+            scan_x += 1
+
+        scan_y = 1
+        while scan_y < self.__y_max:
+            if self.check_above_higher(scan_y, 0) and self.check_right_higher(scan_y, 0) and self.check_below_higher(scan_y, 0):
+                self.__lowpoint_map[scan_y, 0] = self.__smoke_map[scan_y, 0] + 1
+            if self.check_above_higher(scan_y, self.__x_max) and self.check_left_higher(scan_y, self.__x_max) and self.check_below_higher(
+                    scan_y, self.__x_max):
+                self.__lowpoint_map[scan_y, self.__x_max] = self.__smoke_map[scan_y, self.__x_max] + 1
+            scan_y += 1
+
+    def map_corners(self):
+        if self.check_right_higher(0,0) and self.check_below_higher(0,0):
+            self.__lowpoint_map[0,0] = self.__smoke_map[0,0]+1
+        if self.check_left_higher(0,self.__x_max) and self.check_below_higher(0,self.__x_max):
+            self.__lowpoint_map[0,self.__x_max] = self.__smoke_map[0,self.__x_max]+1
+        if self.check_right_higher(self.__y_max,0) and self.check_above_higher(self.__y_max,0):
+            self.__lowpoint_map[self.__y_max,0] = self.__smoke_map[self.__y_max,0]+1
+        if self.check_left_higher(self.__y_max, self.__x_max) and self.check_above_higher(self.__y_max, self.__x_max):
+            self.__lowpoint_map[self.__y_max, self.__x_max] = self.__smoke_map[self.__y_max, self.__x_max] + 1
+
+    def check_above_higher(self,y,x):
+        return self.__smoke_map[y,x]<self.__smoke_map[y-1,x]
+
+    def check_left_higher(self,y,x):
+        return self.__smoke_map[y,x]<self.__smoke_map[y,x-1]
+
+    def check_right_higher(self, y, x):
+        return self.__smoke_map[y, x] < self.__smoke_map[y, x+1]
+
+    def check_below_higher(self, y, x):
+        return self.__smoke_map[y, x] < self.__smoke_map[y +1, x]
 
     # def plot_vertical_line(self, lineCoordinates):
     #     current_y = min(lineCoordinates.get_y1(), lineCoordinates.get_y2())
@@ -139,9 +190,11 @@ class SmokeMap:
 
 
 if __name__ == '__main__':
-    parser = LineParser(True)
+    parser = LineParser()
     smoke_map = SmokeMap(parser)
-    print(smoke_map.get_map())
-    print(np.zeros(smoke_map.get_map().shape, dtype=np.int32))
+    print(smoke_map.get_smoke_map())
+    print(smoke_map.get_lowpoint_map())
+    print(smoke_map.get_lowpoint_map().sum())
+
 
 
