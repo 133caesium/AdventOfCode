c0bc3df9952a0a3d4fe7b437ae9641327dd5c4f5
nber106 <nicholas.bai.berg@gmail.com>
2022-04-12 13:30:44 +1200

    code is now working on 3 test cases

--- AdventOfCode2021/Day12Python/main.py
+++ AdventOfCode2021/Day12Python/main.py
@@ -1,4 +1,5 @@
 import re
+import copy
 
 class LineParser:
 
@@ -7,7 +8,7 @@ class LineParser:
         self.initial_state = []
         cave_lines = []
         for line in input_data:
-            if (line=='\n'):
+            if (line == '\n'):
                 self.initial_state.append(cave_lines)
                 cave_lines = []
             else:
@@ -24,6 +25,7 @@ class LineParser:
         with open(input_path) as file:
             input_lines = file.readlines()
         return input_lines
+
 
 class Cave:
     def __init__(self, id):
@@ -62,17 +64,21 @@ class Cave:
     def __repr__(self):
         return f'Cave-{self.__id}'
 
+
 class Path:
     path_id = 0
 
-    def __init__(self, cave_network_set):
+    def __init__(self, cave_network_set, path_list=['start']):
         self.__id = Path.path_id
         Path.path_id += 1
-        self.__cave_network_set = cave_network_set
-        self.__path_list = ['start']
+        self.__cave_network_set = copy.deepcopy(cave_network_set)
+        self.__path_list = copy.deepcopy(path_list)
+        if len(path_list) == 1:
+            self.__cave_network_set['start'].add_visit
 
     def add_cave_to_path(self, cave_id):
         if cave_id in self.__cave_network_set[self.__path_list[-1]].get_connected_caves():
+            self.__cave_network_set[cave_id].add_visit()
             self.__path_list.append(cave_id)
 
     def get_id(self):
@@ -81,11 +87,23 @@ class Path:
     def is_complete(self):
         return self.__path_list[-1] == 'end'
 
-    def get_visits(self):
-        return self.__times_visited
+    def get_last_cave_id(self):
+        return self.__path_list[-1]
 
-    def get_connected_caves(self):
-        return self.__connected_caves
+    def get_path_list(self):
+        return self.__path_list
+
+    def get_next_caves(self):
+        next_caves = []
+        possible_caves = self.__cave_network_set[self.__path_list[-1]].get_connected_caves()
+        for cave in possible_caves:
+            if self.__cave_network_set[cave].is_big() or self.__cave_network_set[cave].get_visits() == 0:
+                if cave != 'start':
+                    next_caves.append(cave)
+        return next_caves
+
+    def get_cave_network(self):
+        return self.__cave_network_set
 
     def __eq__(self, other):
         if isinstance(other, Path):
@@ -98,6 +116,7 @@ class Path:
 
     def __repr__(self):
         return f'[Path ID: {self.__id}, Path: {"-".join(self.__path_list)}]'
+
 
 def get_cave_set(cave_text):
     caves = {}
@@ -113,29 +132,58 @@ def get_cave_set(cave_text):
         caves[cave2].add_connected_cave(cave1)
     return caves
 
+
 def initalise_caves(verbose=False):
     if verbose:
         print('--------------------------------------------------------------------')
         print('Set-up Caves')
     bulk_cave_text = LineParser(True)
-    individual_cave_text = bulk_cave_text.initial_state[0][1:]
+    individual_cave_text = bulk_cave_text.initial_state[2][1:]
     caves = get_cave_set(individual_cave_text)
     if verbose:
         print(caves)
         for cave in caves:
-            print(f'This cave is called {caves[cave].get_id()} and it`s big status is {caves[cave].is_big()} and it connects to {caves[cave].get_connected_caves()}')
+            print(
+                f'This cave is called {caves[cave].get_id()} and it`s big status is {caves[cave].is_big()} and it connects to {caves[cave].get_connected_caves()}')
         print('--------------------------------------------------------------------')
         print('')
         print('')
     return caves
 
+def grow_paths(paths):
+    next_paths = set()
+    for path in paths:
+        if not path.is_complete():
+            if len(path.get_next_caves())>0:
+                for cave in path.get_next_caves():
+                    new_path = Path(path.get_cave_network(), path.get_path_list())
+                    new_path.add_cave_to_path(cave)
+                    next_paths.add(new_path)
+    return next_paths
+
+
+
 if __name__ == '__main__':
     caves = initalise_caves(True)
 
     first_path = Path(caves)
-    paths = set()
-    paths.add(first_path)
-    for path in paths:
-        path.add_cave_to_path('A')
+    current_paths = set()
+    current_paths.add(first_path)
 
-    print(paths)
+    all_paths = set()
+
+    while len(current_paths)!=0:
+        print('adding paths:')
+        print(current_paths)
+        for path in current_paths:
+            all_paths.add(path)
+        current_paths = grow_paths(current_paths)
+    print('--------------------------------------------------------------------')
+    print('Final list of paths')
+    print(all_paths)
+    valid_paths = set()
+    for path in all_paths:
+        if path.is_complete():
+            valid_paths.add(path)
+    print(f'Final list of valid paths (contains {len(valid_paths)} paths)')
+    print(valid_paths)
