e9e96f22b8597ec6e50e6d64743460e62ac1a5ee
nber106 <nicholas.bai.berg@gmail.com>
2022-04-12 12:59:07 +1200

    added path object and working on path creation

--- AdventOfCode2021/Day12Python/main.py
+++ AdventOfCode2021/Day12Python/main.py
@@ -62,6 +62,43 @@ class Cave:
     def __repr__(self):
         return f'Cave-{self.__id}'
 
+class Path:
+    path_id = 0
+
+    def __init__(self, cave_network_set):
+        self.__id = Path.path_id
+        Path.path_id += 1
+        self.__cave_network_set = cave_network_set
+        self.__path_list = ['start']
+
+    def add_cave_to_path(self, cave_id):
+        if cave_id in self.__cave_network_set[self.__path_list[-1]].get_connected_caves():
+            self.__path_list.append(cave_id)
+
+    def get_id(self):
+        return self.__id
+
+    def is_complete(self):
+        return self.__path_list[-1] == 'end'
+
+    def get_visits(self):
+        return self.__times_visited
+
+    def get_connected_caves(self):
+        return self.__connected_caves
+
+    def __eq__(self, other):
+        if isinstance(other, Path):
+            return self.__hash__() == other.__hash__()
+        else:
+            return False
+
+    def __hash__(self):
+        return hash(''.join(self.__path_list))
+
+    def __repr__(self):
+        return f'[Path ID: {self.__id}, Path: {"-".join(self.__path_list)}]'
+
 def get_cave_set(cave_text):
     caves = {}
     for connection in cave_text:
@@ -74,17 +111,31 @@ def get_cave_set(cave_text):
             caves[cave2] = new_cave2
         caves[cave1].add_connected_cave(cave2)
         caves[cave2].add_connected_cave(cave1)
-
     return caves
 
-def initalise_caves():
+def initalise_caves(verbose=False):
+    if verbose:
+        print('--------------------------------------------------------------------')
+        print('Set-up Caves')
     bulk_cave_text = LineParser(True)
     individual_cave_text = bulk_cave_text.initial_state[0][1:]
     caves = get_cave_set(individual_cave_text)
-    print(caves)
+    if verbose:
+        print(caves)
+        for cave in caves:
+            print(f'This cave is called {caves[cave].get_id()} and it`s big status is {caves[cave].is_big()} and it connects to {caves[cave].get_connected_caves()}')
+        print('--------------------------------------------------------------------')
+        print('')
+        print('')
     return caves
 
 if __name__ == '__main__':
-    caves = initalise_caves()
-    for cave in caves:
-        print(f'This cave is called {caves[cave].get_id()} and it`s big status is {caves[cave].is_big()} and it connects to {caves[cave].get_connected_caves()}')
+    caves = initalise_caves(True)
+
+    first_path = Path(caves)
+    paths = set()
+    paths.add(first_path)
+    for path in paths:
+        path.add_cave_to_path('A')
+
+    print(paths)
