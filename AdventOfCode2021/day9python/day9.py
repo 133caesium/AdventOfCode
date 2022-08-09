abadc53f79b2906772ae2620db9a47d86e82a741
nber106 <nicholas.bai.berg@gmail.com>
2022-08-09 17:22:08 +1200

    not an elegant solution, but a solution

--- AdventOfCode2021/day9python/day9.py
+++ AdventOfCode2021/day9python/day9.py
@@ -1,6 +1,7 @@
 import numpy as np
 from greyscaleplot import plot_numpy_matrix, plot_n_numpy_matricies
 
+
 # Note to self, this might be one solution:
 # https://en.wikipedia.org/wiki/Flood_fill
 
@@ -30,6 +31,15 @@ class LineParser:
         return input_lines
 
 
+class Basin:
+    basinCount = 1
+
+    def __init__(self):
+        self.basinNumber = self.basinCount
+        self.basinCoordinatesList = {}
+        Basin.basinCount = Basin.basinCount + 1
+
+
 class SmokeMap:
     def __init__(self, parser):
         self.__smoke_map = np.matrix(parser.get_initial_state())
@@ -37,9 +47,11 @@ class SmokeMap:
         self.__basin_map = np.zeros(self.__smoke_map.shape, dtype=np.int32)
         self.__y_max = self.__smoke_map.shape[0] - 1
         self.__x_max = self.__smoke_map.shape[1] - 1
+        self.__lowpoint_count = 0
+        self.list_of_basin_sizes = []
+        self.current_basin_size = 0
         self.map_lowpoints()
         self.map_basin()
-        self.__lowpoint_count = 0
 
 
     def get_smoke_map(self):
@@ -51,10 +63,12 @@ class SmokeMap:
     def get_basin_map(self):
         return self.__basin_map
 
-    def map_basin(self):
-        #if the cell has value 9, it is not in a basin
-        #if the cell has an adjacent value that is in a basin, take that value
-        #otherwise, increment the basin counter and that cell is in a basin.
+    # This map_basin function is very bad. Does not produced the desired outcome and is not that easy to understand.
+    # I have since learned much nicer ways to handle this in Python.
+    def map_basin_old(self):
+        # if the cell has value 9, it is not in a basin
+        # if the cell has an adjacent value that is in a basin, take that value
+        # otherwise, increment the basin counter and that cell is in a basin.
 
         changed_something = False
         basin_count = 1
@@ -65,20 +79,63 @@ class SmokeMap:
                 if self.__smoke_map[scan_y, scan_x] == 9:
                     self.__basin_map[scan_y, scan_x] = -1
                 if self.__smoke_map[scan_y, scan_x] != 9:
-                    if self.__basin_map[scan_y-1, scan_x] > 0:
-                        self.__basin_map[scan_y, scan_x] = self.__basin_map[scan_y-1, scan_x]
-                    elif self.__basin_map[scan_y, scan_x-1] > 0:
-                        self.__basin_map[scan_y, scan_x] = self.__basin_map[scan_y, scan_x-1]
+                    if self.__basin_map[scan_y - 1, scan_x] > 0:
+                        self.__basin_map[scan_y, scan_x] = self.__basin_map[scan_y - 1, scan_x]
+                    elif self.__basin_map[scan_y, scan_x - 1] > 0:
+                        self.__basin_map[scan_y, scan_x] = self.__basin_map[scan_y, scan_x - 1]
                     else:
                         self.__basin_map[scan_y, scan_x] = basin_count
                         basin_count += 1
                 scan_y += 1
             scan_x += 1
 
+    def map_basin(self):
+        basin_count = 1
+        scan_x = 0
+        # Not elegant, but map the walls first.
+        while scan_x <= self.__x_max:
+            scan_y = 0
+            while scan_y <= self.__y_max:
+                if self.__smoke_map[scan_y, scan_x] == 9:
+                    self.__basin_map[scan_y, scan_x] = -1
+                scan_y += 1
+            scan_x += 1
+        # Not elegant, then map the basins
+        scan_x = 0
+        while scan_x <= self.__x_max:
+            scan_y = 0
+            while scan_y <= self.__y_max:
+                if self.__lowpoint_map[scan_y, scan_x] != 0:
+                    basin_count = basin_count + 1
+                    self.current_basin_size = 0
+                    # self.__basin_map[scan_y, scan_x] = basin_count
+                    self.flood_fill(scan_y, scan_x, 0, basin_count)
+                    self.list_of_basin_sizes.append(self.current_basin_size)
+                    # basin_count = basin_count + 1
+                scan_y += 1
+            scan_x += 1
+        print(self.list_of_basin_sizes)
+        self.list_of_basin_sizes.sort()
+        print(self.list_of_basin_sizes)
+
+
+
+    def flood_fill(self, y, x, search, replace):
+        if x < 0 or x > self.__x_max or y < 0 or y > self.__y_max:
+            return False
+        if self.__basin_map[y, x] != search:
+            return False
+        self.__basin_map[y, x] = replace
+        self.current_basin_size = self.current_basin_size + 1
+        self.flood_fill(y + 1, x, search, replace)
+        self.flood_fill(y - 1, x, search, replace)
+        self.flood_fill(y, x + 1, search, replace)
+        self.flood_fill(y, x - 1, search, replace)
+
+
     def check_cells_basin(self):
         pass
-        #check cell values
-
+        # check cell values
 
     def map_lowpoints(self):
         self.map_corners()
@@ -90,7 +147,9 @@ class SmokeMap:
         while scan_x < self.__x_max:
             scan_y = 1
             while scan_y < self.__y_max:
-                if self.check_above_higher(scan_y, scan_x) and self.check_left_higher(scan_y, scan_x) and self.check_right_higher(scan_y, scan_x) and self.check_below_higher(scan_y, scan_x):
+                if self.check_above_higher(scan_y, scan_x) and self.check_left_higher(scan_y,
+                                                                                      scan_x) and self.check_right_higher(
+                        scan_y, scan_x) and self.check_below_higher(scan_y, scan_x):
                     self.__lowpoint_map[scan_y, scan_x] = self.__smoke_map[scan_y, scan_x] + 1
                 scan_y += 1
             scan_x += 1
@@ -98,42 +157,47 @@ class SmokeMap:
     def map_edges(self):
         scan_x = 1
         while scan_x < self.__x_max:
-            if self.check_left_higher(0, scan_x) and self.check_right_higher(0, scan_x) and self.check_below_higher(0, scan_x):
+            if self.check_left_higher(0, scan_x) and self.check_right_higher(0, scan_x) and self.check_below_higher(0,
+                                                                                                                    scan_x):
                 self.__lowpoint_map[0, scan_x] = self.__smoke_map[0, scan_x] + 1
-            if self.check_left_higher(self.__y_max, scan_x) and self.check_right_higher(self.__y_max, scan_x) and self.check_above_higher(self.__y_max, scan_x):
+            if self.check_left_higher(self.__y_max, scan_x) and self.check_right_higher(self.__y_max,
+                                                                                        scan_x) and self.check_above_higher(
+                    self.__y_max, scan_x):
                 self.__lowpoint_map[self.__y_max, scan_x] = self.__smoke_map[self.__y_max, scan_x] + 1
             scan_x += 1
 
         scan_y = 1
         while scan_y < self.__y_max:
-            if self.check_above_higher(scan_y, 0) and self.check_right_higher(scan_y, 0) and self.check_below_higher(scan_y, 0):
+            if self.check_above_higher(scan_y, 0) and self.check_right_higher(scan_y, 0) and self.check_below_higher(
+                    scan_y, 0):
                 self.__lowpoint_map[scan_y, 0] = self.__smoke_map[scan_y, 0] + 1
-            if self.check_above_higher(scan_y, self.__x_max) and self.check_left_higher(scan_y, self.__x_max) and self.check_below_higher(
+            if self.check_above_higher(scan_y, self.__x_max) and self.check_left_higher(scan_y,
+                                                                                        self.__x_max) and self.check_below_higher(
                     scan_y, self.__x_max):
                 self.__lowpoint_map[scan_y, self.__x_max] = self.__smoke_map[scan_y, self.__x_max] + 1
             scan_y += 1
 
     def map_corners(self):
-        if self.check_right_higher(0,0) and self.check_below_higher(0,0):
-            self.__lowpoint_map[0,0] = self.__smoke_map[0,0]+1
-        if self.check_left_higher(0,self.__x_max) and self.check_below_higher(0,self.__x_max):
-            self.__lowpoint_map[0,self.__x_max] = self.__smoke_map[0,self.__x_max]+1
-        if self.check_right_higher(self.__y_max,0) and self.check_above_higher(self.__y_max,0):
-            self.__lowpoint_map[self.__y_max,0] = self.__smoke_map[self.__y_max,0]+1
+        if self.check_right_higher(0, 0) and self.check_below_higher(0, 0):
+            self.__lowpoint_map[0, 0] = self.__smoke_map[0, 0] + 1
+        if self.check_left_higher(0, self.__x_max) and self.check_below_higher(0, self.__x_max):
+            self.__lowpoint_map[0, self.__x_max] = self.__smoke_map[0, self.__x_max] + 1
+        if self.check_right_higher(self.__y_max, 0) and self.check_above_higher(self.__y_max, 0):
+            self.__lowpoint_map[self.__y_max, 0] = self.__smoke_map[self.__y_max, 0] + 1
         if self.check_left_higher(self.__y_max, self.__x_max) and self.check_above_higher(self.__y_max, self.__x_max):
             self.__lowpoint_map[self.__y_max, self.__x_max] = self.__smoke_map[self.__y_max, self.__x_max] + 1
 
-    def check_above_higher(self,y,x):
-        return self.__smoke_map[y,x]<self.__smoke_map[y-1,x]
+    def check_above_higher(self, y, x):
+        return self.__smoke_map[y, x] < self.__smoke_map[y - 1, x]
 
-    def check_left_higher(self,y,x):
-        return self.__smoke_map[y,x]<self.__smoke_map[y,x-1]
+    def check_left_higher(self, y, x):
+        return self.__smoke_map[y, x] < self.__smoke_map[y, x - 1]
 
     def check_right_higher(self, y, x):
-        return self.__smoke_map[y, x] < self.__smoke_map[y, x+1]
+        return self.__smoke_map[y, x] < self.__smoke_map[y, x + 1]
 
     def check_below_higher(self, y, x):
-        return self.__smoke_map[y, x] < self.__smoke_map[y +1, x]
+        return self.__smoke_map[y, x] < self.__smoke_map[y + 1, x]
 
 
 def archived_function_calls():
@@ -147,12 +211,11 @@ def archived_function_calls():
 
 
 if __name__ == '__main__':
-    parser = LineParser(False)
+    parser = LineParser(True)
     smoke_map = SmokeMap(parser)
+
     smoke_map_matrix = smoke_map.get_smoke_map()
     lowpoint_matrix = smoke_map.get_lowpoint_map()
     basin_matrix = smoke_map.get_basin_map()
     plot_n_numpy_matricies(smoke_map_matrix, lowpoint_matrix, basin_matrix)
-
-
-
+    print(smoke_map.get_basin_map())
