24879596144ce39cddd55b94625369229f4a9d2c
nber106 <nicholas.bai.berg@gmail.com>
2022-11-21 12:21:59 +1300

    Solve Day 3 Part 2

--- AdventOfCode2015Python/Day03/Day03.py
+++ AdventOfCode2015Python/Day03/Day03.py
@@ -1,3 +1,5 @@
+import copy
+
 from Utilities.Utilities import LineParser
 
 testing = True
@@ -17,46 +19,42 @@ class CartesianLocation:
     def __hash__(self):
         return hash(f'{self.x},{self.y}')
 
+    def move(self, move):
+        if move == '^':
+            self.y += 1
+        elif move == 'v':
+            self.y -= 1
+        elif move == '>':
+            self.x += 1
+        elif move == '<':
+            self.x -= 1
+        else:
+            print('Error, unexpected move')
+
+
 #     https://www.singlelunch.com/2018/09/26/programming-trick-cantor-pairing-perfect-hashing-of-two-integers/
 
 
 if __name__ == '__main__':
-    print(f'We will test {len(raw_input.get_initial_state()[0])} locations')
-    x = 0
-    y = 0
-    visited_locations = set()
-    for move in raw_input.get_initial_state()[0][::2]:
-        if move == '^':
-            y += 1
-        elif move == 'v':
-            y -= 1
-        elif move == '>':
-            x += 1
-        elif move == '<':
-            x -= 1
-        else:
-            print('Error, unexpected move')
-        current_location = CartesianLocation(x,y)
-        # print(f'Move [{move}] gives {CartesianLocation(x,y)}')
-        visited_locations.add(current_location)
-        # print(visited_locations)
-        # print(f'We have visited {len(visited_locations)} locations')
+    print(f'We will test {len(raw_input.get_initial_state()[0])} moves')
 
-    print(visited_locations)
-    print(f'We have visited {len(visited_locations)} locations')
+    # Day 03 Part 1
+    santa = CartesianLocation(0, 0)
+    visited_locations = {santa}
+    for move in raw_input.get_initial_state()[0]:
+        santa.move(move)
+        visited_locations.add(copy.copy(santa))
+    print(f'Day03 Part 1: We have visited {len(visited_locations)} locations')
 
-    # for letter in raw_input.get_initial_state()[0]:
-    #     if (letter == '('):
-    #         initial_level += 1
-    #     else:
-    #         initial_level -= 1
-    # print(initial_level)
-    # index = 0
-    # initial_level = 0
-    # while initial_level >=0:
-    #     if (raw_input.get_initial_state()[0][index] == '('):
-    #         initial_level += 1
-    #     else:
-    #         initial_level -= 1
-    #     index += 1
-    # print(index)
+    # Day 03 Part 2
+    santa = CartesianLocation(0, 0)
+    roboSanta = CartesianLocation(0, 0)
+    visited_locations = {santa}
+    for move in raw_input.get_initial_state()[0][0::2]:
+        santa.move(move)
+        visited_locations.add(copy.copy(santa))
+    for move in raw_input.get_initial_state()[0][1::2]:
+        roboSanta.move(move)
+        visited_locations.add(copy.copy(roboSanta))
+    print(f'Day03 Part 2: We have visited {len(visited_locations)} locations')
+
