d600b36d31c5573bf376c9c3fca8caaef46eff37
nber106 <nicholas.bai.berg@gmail.com>
2022-12-01 23:54:32 +1300

    2022Day01: Solve day 01

--- AdventOfCode2022Typescript/Day01/Day01.js
+++ AdventOfCode2022Typescript/Day01/Day01.js
@@ -1,8 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs_1 = require("fs");
-var message = "Hello world";
-console.log(message);
-var file = (0, fs_1.readFileSync)('./Day01/input.txt', 'utf-8');
-console.log(file);
+var ElfFood = /** @class */ (function () {
+    function ElfFood(foodItems) {
+        this.foodItems = [];
+        this.foodItems = foodItems;
+        this.foodTotal = foodItems.reduce(function (sum, current) { return sum + current; }, 0);
+    }
+    ElfFood.prototype.print = function () {
+        return "Hello, this elf has ".concat(this.foodTotal, " calories from ").concat(this.foodItems);
+    };
+    return ElfFood;
+}());
+var rawFile = (0, fs_1.readFileSync)('./Day01/input.txt', 'utf-8');
+var fileAsNumericArray = rawFile.split("\n").map(function (str) { return parseInt(str); });
+var bag = [];
+var elves = [];
+var maxElf = 0;
+for (var _i = 0, fileAsNumericArray_1 = fileAsNumericArray; _i < fileAsNumericArray_1.length; _i++) {
+    var item = fileAsNumericArray_1[_i];
+    if (Number.isNaN(item)) {
+        var newElf = new ElfFood(bag);
+        elves.push(newElf);
+        if (newElf.foodTotal > maxElf) {
+            maxElf = newElf.foodTotal;
+        }
+        ;
+        bag = [];
+    }
+    else {
+        bag.push(item);
+    }
+}
+if (bag.length > 0) {
+    elves.push(new ElfFood(bag));
+}
+console.log(elves);
+console.log("The most food carried was ".concat(maxElf));
 //# sourceMappingURL=Day01.js.map
--- AdventOfCode2022Typescript/Day01/Day01.ts
+++ AdventOfCode2022Typescript/Day01/Day01.ts
@@ -1,8 +1,36 @@
 import { readFileSync } from 'fs';
 
+class ElfFood {
+    foodItems: number[] = [];
+    foodTotal: number;
+   
+    constructor(foodItems: number[]) {
+      this.foodItems = foodItems;
+      this.foodTotal = foodItems.reduce((sum, current) => sum + current, 0);
+    }
+   
+    print() {
+      return `Hello, this elf has ${this.foodTotal} calories from ${this.foodItems}`;
+    }
+  }
 
-let message: string = "Hello world";
-console.log(message)
-
-const file = readFileSync('./Day01/input.txt', 'utf-8');
-console.log(file)
+const rawFile = readFileSync('./Day01/input.txt', 'utf-8');
+const fileAsNumericArray = rawFile.split("\n").map(str => {return parseInt(str)});
+let bag: number[] = [];
+let elves: ElfFood[] = [];
+let maxElf = 0;
+for (let item of fileAsNumericArray) {
+    if (Number.isNaN(item)) {
+        let newElf = new ElfFood(bag)
+        elves.push(newElf)
+        if(newElf.foodTotal>maxElf){maxElf = newElf.foodTotal};
+        bag = [];
+    } else {
+        bag.push(item)
+    }
+}
+if (bag.length > 0) {
+    elves.push(new ElfFood(bag))
+}
+console.log(elves)
+console.log(`The most food carried was ${maxElf}`)
