83d06d9fbd51a946f3999ea60eec3a63b328b715
nber106 <nicholas.bai.berg@gmail.com>
2022-12-03 22:15:14 +1300

    2022Day03: Solve day03 part 2 Code is tolerable level of messy for resuse later

--- AdventOfCode2022Typescript/Day03/Day03.ts
+++ AdventOfCode2022Typescript/Day03/Day03.ts
@@ -39,18 +39,78 @@ class ElfBackpack {
     }
   }
 
+  class ElfBadgeGroup {
+    backpack1: string;
+    backpack2: string;
+    backpack3: string;
+    duplicateCharacters: string[];
+    duplicateCharValue: number;
+   
+    constructor(backpack1: string, backpack2: string, backpack3: string) {
+      this.backpack1 = backpack1;
+      this.backpack2 = backpack2;
+      this.backpack3 = backpack3;
+      this.duplicateCharacters = [];
+      this.findDuplicates()
+      this.duplicateCharValue =  (this.duplicateCharacters[0].charCodeAt(0) -64)<27 ? 
+        this.duplicateCharacters[0].charCodeAt(0) -64+26 :
+        this.duplicateCharacters[0].charCodeAt(0) -64-32;
+    }
+
+    findDuplicates() {
+        for (var i = 0; i < this.backpack1.length; i++) {
+            if (this.backpack2.includes(this.backpack1.charAt(i))) {
+                if (this.backpack3.includes(this.backpack1.charAt(i))) {
+                    this.duplicateCharacters.push(this.backpack1.charAt(i))
+                }
+            }
+        }
+    }
+   
+    toString() {
+      return (
+        `A trio of elf backpacks containing:\n`+
+        `${this.backpack1}\n`+
+        `${this.backpack2}\n`+
+        `${this.backpack3}\n` +
+        `Duplicate characters \n` +
+        `${this.duplicateCharacters} \n`)+
+        `By default this has the value ${
+           this.duplicateCharValue}`
+    }
+  }
+
+
 function importData(): string[] {
     const rawFile = readFileSync('./Day03/input.txt', 'utf-8');
     const fileAsStringArray = rawFile.split("\r\n");
     return fileAsStringArray;
 }
 
-const data = importData()
-console.log(data)
-let backpackDuplicatesum = 0;
-for (let individualBackpackRaw of data) {
-    const backpack = new ElfBackpack(individualBackpackRaw)
-    console.log(backpack.toString())
-    backpackDuplicatesum = backpackDuplicatesum + backpack.duplicateCharValue
+function solvePart1(data: string[]): number {
+    let backpackDuplicatesum = 0;
+    for (let individualBackpackRaw of data) {
+        const backpack = new ElfBackpack(individualBackpackRaw)
+        // console.log(backpack.toString())
+        backpackDuplicatesum = backpackDuplicatesum + backpack.duplicateCharValue
+    }
+    console.log(`Final sum is ${backpackDuplicatesum}`)
+    return backpackDuplicatesum;
 }
-console.log(`Final sum is ${backpackDuplicatesum}`)
+
+function solvePart2(data: string[]): number {
+    let badgeSum = 0;
+    for (var i = 0; i < data.length; i=i+3) {
+        const badgeGroup = new ElfBadgeGroup(data[i], data[i+1], data[i+2])
+        // console.log(badgeGroup.toString())
+        badgeSum = badgeSum + badgeGroup.duplicateCharValue
+    }
+    console.log(`Final sum is ${badgeSum}`)
+    return(badgeSum)           
+}
+
+const data = importData()
+solvePart1(data)
+solvePart2(data)
+
+
