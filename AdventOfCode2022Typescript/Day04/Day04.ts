79634f849baf6ad8d85b72d8bd836b05664355b6
nber106 <nicholas.bai.berg@gmail.com>
2022-12-04 23:47:42 +1300

    2022Day04: Solve part 1 Code has improved with the time setting up eslint and prettier

---
+++ AdventOfCode2022Typescript/Day04/Day04.ts
@@ -0,0 +1,61 @@
+import { readFileSync } from 'fs';
+
+class ElfPair {
+  elf1start: number;
+  elf1end: number;
+  elf2start: number;
+  elf2end: number;
+  anElfIsContained: boolean;
+
+  constructor(data: string) {
+    this.elf1start = parseInt(data.split(',')[0].split('-')[0]);
+    this.elf1end = parseInt(data.split(',')[0].split('-')[1]);
+    this.elf2start = parseInt(data.split(',')[1].split('-')[0]);
+    this.elf2end = parseInt(data.split(',')[1].split('-')[1]);
+    this.anElfIsContained = this.checkContainment();
+  }
+
+  checkContainment(): boolean {
+    return (
+      (this.elf1start <= this.elf2start && this.elf2end <= this.elf1end) ||
+      (this.elf1start >= this.elf2start && this.elf2end >= this.elf1end)
+    );
+  }
+
+  toString(): string {
+    return (
+      `A pair of elves who are cleaning:\n` +
+      `${this.elf1start} to ${this.elf1end} and\n` +
+      `${this.elf2start} to ${this.elf2end}\n` +
+      (this.anElfIsContained
+        ? `One's range contains the other`
+        : `No containment`)
+    );
+  }
+}
+
+function importData(): string[] {
+  const rawFile = readFileSync('./Day04/sample_input.txt', 'utf-8');
+  const fileAsStringArray = rawFile.split('\r\n');
+  return fileAsStringArray;
+}
+
+function solvePart1(data: string[]): number {
+  let containmentCount = 0;
+  for (const elfRangeRaw of data) {
+    const elfRange = new ElfPair(elfRangeRaw);
+    if (elfRange.anElfIsContained) {
+      containmentCount++
+    }
+  }
+  console.log(`There were ${containmentCount} pairs that had containment`)
+  return containmentCount;
+}
+
+function solvePart2(data: string[]): number {
+  return 0;
+}
+
+const data = importData();
+solvePart1(data);
+solvePart2(data);
