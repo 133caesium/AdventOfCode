2620b4d7cedcab2d0735b6fc808bc857b6bb7d7a
nber106 <nicholas.bai.berg@gmail.com>
2022-12-04 23:42:21 +1300

    Linting: format with prettier

--- AdventOfCode2022Typescript/GenericStart.ts
+++ AdventOfCode2022Typescript/GenericStart.ts
@@ -1,38 +1,32 @@
 import { readFileSync } from 'fs';
 
 class GenericClass {
-    stringThing: string;
-    numberThing: number;
-   
-    constructor(data: string) {
-      this.stringThing = data;
-    }
-   
-    toString(): string {
-      return (
-        `A string version of the object`
-      )
-    }
+  stringThing: string;
+  numberThing: number;
+
+  constructor(data: string) {
+    this.stringThing = data;
   }
 
-
+  toString(): string {
+    return `A string version of the object`;
+  }
+}
 
 function importData(): string[] {
-    const rawFile = readFileSync('./Day0X/sample_input.txt', 'utf-8');
-    const fileAsStringArray = rawFile.split("\r\n");
-    return fileAsStringArray;
+  const rawFile = readFileSync('./Day0X/sample_input.txt', 'utf-8');
+  const fileAsStringArray = rawFile.split('\r\n');
+  return fileAsStringArray;
 }
 
 function solvePart1(data: string[]): number {
-    return 0;
+  return 0;
 }
 
 function solvePart2(data: string[]): number {
-    return 0;           
+  return 0;
 }
 
-const data = importData()
-solvePart1(data)
-solvePart2(data)
-
-
+const data = importData();
+solvePart1(data);
+solvePart2(data);
--- AdventOfCode2022Typescript/README.md
+++ AdventOfCode2022Typescript/README.md
@@ -1,5 +1,9 @@
 # Advent of Code 2022 Typescript
+
 Compiling Typescript
 
-Compile 
-```yarn tsc Day03/Day03.ts```
+Compile
+`yarn tsc Day03/Day03.ts`
+
+Tidy up code with linting
+`yarn prettier --write Day04/Day04.ts`
