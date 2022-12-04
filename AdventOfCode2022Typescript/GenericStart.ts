08d0fe025f5e7675007c4ef85b973e75759b9c6f
nber106 <nicholas.bai.berg@gmail.com>
2022-12-04 22:35:25 +1300

    add a boilerplate file for future days

---
+++ AdventOfCode2022Typescript/GenericStart.ts
@@ -0,0 +1,37 @@
+import { readFileSync } from 'fs';
+
+class GenericClass {
+    stringThing: string;
+    numberThing: Number;
+   
+    constructor(data: string) {
+    }
+   
+    toString() {
+      return (
+        `A string version of the object`
+      )
+    }
+  }
+
+
+
+function importData(): string[] {
+    const rawFile = readFileSync('./Day0X/sample_input.txt', 'utf-8');
+    const fileAsStringArray = rawFile.split("\r\n");
+    return fileAsStringArray;
+}
+
+function solvePart1(data: string[]): number {
+    return 0;
+}
+
+function solvePart2(data: string[]): number {
+    return 0;           
+}
+
+const data = importData()
+solvePart1(data)
+solvePart2(data)
+
+
