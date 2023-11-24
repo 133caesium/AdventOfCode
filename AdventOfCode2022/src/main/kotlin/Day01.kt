3f730978cbe5c4b7ca11883cdf80939653b5ca4d
nber106 <nicholas.bai.berg@gmail.com>
2023-11-24 16:36:55 +1300

    remove duplication 2022 day 1

--- AdventOfCode2022/src/main/kotlin/Day01.kt
+++ AdventOfCode2022/src/main/kotlin/Day01.kt
@@ -1,25 +1,31 @@
 import java.io.File
 
 fun main() {
-    fun parseInput(input: String): List<Int> {
+    fun parseInput(input: String): List<List<Int>> {
         val data = input.split("\r\n\r\n").map { elf ->
-            elf.lines().sumOf { it.toInt() }
+            elf.lines().map { it.toInt() }
         }
         return data
     }
 
+    fun List<List<Int>>.topNElves(n:Int): Int {
+        return map {it.sum() }
+        .sortedDescending()
+        .take(n)
+        .sum()
+    }
+
     fun part1(input: String): Int {
         val data = parseInput(input)
-        return data.maxOrNull() ?: 0
+        return data.topNElves(1)
     }
 
     fun part2(input:String): Int{
         val data = parseInput(input)
-        return data
-            .sortedDescending()
-            .take(3)
-            .sum()
+        return data.topNElves(3)
     }
+
+//    fun part2Optimised()
 
     val testInput = File("src/Day01_test.txt").readText()
     check(part1(testInput) == 24000)
