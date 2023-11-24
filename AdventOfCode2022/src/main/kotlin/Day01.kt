cf2648518ee46752862ea814aeee855dfa8b48bc
nber106 <nicholas.bai.berg@gmail.com>
2023-11-24 16:23:37 +1300

    Solve Day01 part 2

--- AdventOfCode2022/src/main/kotlin/Day01.kt
+++ AdventOfCode2022/src/main/kotlin/Day01.kt
@@ -1,22 +1,32 @@
 import java.io.File
 
 fun main() {
-    fun part1(input: String): Int {
-        val data = input.split("\r\n\r\n").map{ elf ->
-            elf.lines().map{ it.toInt() }
+    fun parseInput(input: String): List<Int> {
+        val data = input.split("\r\n\r\n").map { elf ->
+            elf.lines().sumOf { it.toInt() }
         }
-        return data.maxOf { it.sum() }
+        return data
     }
 
-    fun part2(input: List<String>): Int{
-        return input.size
+    fun part1(input: String): Int {
+        val data = parseInput(input)
+        return data.maxOrNull() ?: 0
+    }
+
+    fun part2(input:String): Int{
+        val data = parseInput(input)
+        return data
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     val testInput = File("src/Day01_test.txt").readText()
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = File("src/Day01.txt").readText()
     val part1Solution = part1(input)
     println("The solution to Part 1 is $part1Solution")
-//    println(part2(input))
+    println("The solution to Part 1 is "+part2(input))
 }
