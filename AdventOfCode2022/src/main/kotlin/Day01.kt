034f9af3ae209b589a2376eab7b6eea0b21ebd0a
nber106 <nicholas.bai.berg@gmail.com>
2023-11-24 16:07:59 +1300

    2022 Kotlin solve part 1

--- AdventOfCode2022/src/main/kotlin/Day01.kt
+++ AdventOfCode2022/src/main/kotlin/Day01.kt
@@ -1,2 +1,22 @@
-class Day01 {
+import java.io.File
+
+fun main() {
+    fun part1(input: String): Int {
+        val data = input.split("\r\n\r\n").map{ elf ->
+            elf.lines().map{ it.toInt() }
+        }
+        return data.maxOf { it.sum() }
+    }
+
+    fun part2(input: List<String>): Int{
+        return input.size
+    }
+
+    val testInput = File("src/Day01_test.txt").readText()
+    check(part1(testInput) == 24000)
+
+    val input = File("src/Day01.txt").readText()
+    val part1Solution = part1(input)
+    println("The solution to Part 1 is $part1Solution")
+//    println(part2(input))
 }
--- AdventOfCode2022/src/main/kotlin/Utils.kt
+++ AdventOfCode2022/src/main/kotlin/Utils.kt
@@ -1,2 +1,13 @@
-class Utils {
-}
+import java.io.File
+import java.math.BigInteger
+import java.security.MessageDigest
+
+
+
+
+fun readInput(name: String) = File("src", "$name.txt")
+    .readLines()
+
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+    .toString(16)
+    .padStart(32,'0')
