48c7e7256a6b20bb7464dcbbac719f740f5ced47
nber106 <nicholas.bai.berg@gmail.com>
2023-11-28 17:02:45 +1300

    Solve 2022 Day 2 part 1 using Kotlin enum

--- AdventOfCode2022/src/main/kotlin/Day02.kt
+++ AdventOfCode2022/src/main/kotlin/Day02.kt
@@ -1,4 +1,35 @@
+import java.io.File
+
 //https://adventofcode.com/2022/day/2
 
-class Day02 {
+enum class Round(val score: Int) {
+    AX(3+1),
+    AY(6+2),
+    AZ(0+3),
+    BX(0+1),
+    BY(3+2),
+    BZ(6+3),
+    CX(6+1),
+    CY(0+2),
+    CZ(3+3),
+}
+
+fun main() {
+    val input = File("src/Day02.txt").readText()
+    val testInput = File("src/Day02_test.txt").readText()
+
+    fun parseInput(input: String): List<Round> {
+        return input.split("\r\n").map {
+            Round.valueOf(it.replace(" ",""))
+        }
+    }
+
+    fun part1(input: String): Int {
+        val data = parseInput(input)
+        return data.map { it.score }.sum()
+    }
+
+    check(part1(testInput) == 15)
+    println("Part 1 Solution: "+part1(input))
+
 }
